5fef5bd6e1fa3d0d4e241be025129aab7d008645
Ignacio Searles <nachosearles3@gmail.com>
2022-11-21 12:08:52 -0300

    improved division example

--- examples/divide.asm
+++ examples/divide.asm
@@ -1,38 +1,44 @@
-org 10
+org 2                   #Start position of instructions
 
-start:
-ld SP, stack
+divide:                 #Divide subrutine
+pop DE                  #pop return address
+pop BC                  #store dividend in B and divisor in C
+push AF                 #save accumulator/flags
 
-ld B, 5
-ld C, 2
+ld A, B                 #load dividend into A
+ld B, 0                 #set quotient to 0
 
-push BC
-call divide
-
-ld (result), A
-int 80h
-
-divide:
-pop DE
-pop BC
-
-ld A, B
-ld B, 0
-
-loop:
-cp C
+loop:                   
+cp C                    #check if divisor is bigger than dividend
 jp M, done
 
-sub C
+sub C                   #if dividend is bigger than divisor subtract and increment quotient
 inc B
 jp loop
 
 done:
+ld (remainder), A      #if divisor is bigger than dividend save remainder and quotient to mem
 ld A, B
+ld (result), A
 
+pop AF                 #restore accumulator/flags and return
 push DE
 ret
 
+start:
+ld SP, stack           #initialize stack
+
+ld B, 89               #dividend
+ld C, 7                #divisor
+
+push BC                #call divide subrutine
+call divide
+
+int 80h                #stop execution
+
+end start              #PC starts at start tag
+
+result: db 0
+remainder: db 0
 ds 20
 stack:
-result: db 0
