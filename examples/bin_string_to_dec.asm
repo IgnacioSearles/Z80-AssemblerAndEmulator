9818d7161dc4fe2a1bd2ab3394472efea11b02f8
Ignacio Searles <nachosearles3@gmail.com>
2022-12-09 15:33:09 -0300

    added comments to example

--- examples/bin_string_to_dec.asm
+++ examples/bin_string_to_dec.asm
@@ -2,46 +2,46 @@ org 0h
 
 start:
 ld B, 0
-ld D, 1
-ld E, 0
+ld D, 1                  ;save amount of shifts per loop in D
+ld E, 0                  ;use E to hold result
 
-ld IX, input_string
+ld IX, input_string      ;load pos of end of input + 1 into IX
 
 loop:
 dec IX
 
-ld A, (IX)
+ld A, (IX)               ;load char into A
 
-cp 0
+cp 0                     ;if reach start of input end
 jp Z, end_loop
 
-sub 30h 
+sub 30h                  ;ascii "0"/"1" -> decimal
 
-ld B, D 
+ld B, D                  ;load necesarry shifts into B                  
 
 shift_a:
-sla A
+sla A                    ;shift number to add
 djnz shift_a
 
-inc D
+inc D                    ;increment shifts for next loop
 
-ld C, A
-ld A, E
+ld C, A                  ;load number into C
+ld A, E                  ;load result into A
 
-add A, C
+add A, C                    ;add number into result
 ld E, A
-jp loop
+jp loop                     ;next char
 
 end_loop:
 ld A, E
-srl A
-ld (result), A
+srl A                       ;shift right
+ld (result), A              ;save result
 
-rst 38h
+rst 38h                     ;halt
 
-end start
+end start                   ;start execution in start label
 
-result: db 0
-db 0
-defm "11011"
-input_string:
+result: db 0                
+db 0                        ;start input with 0, "null started"
+defm "1000101"
+input_string:               ;load end + 1 of input_string
