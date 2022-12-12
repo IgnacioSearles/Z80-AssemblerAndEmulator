458e1adcfcb328b6b0abaee056d81dc053112d31
Ignacio Searles <nachosearles3@gmail.com>
2022-12-12 09:41:22 -0300

    fix comment bug

--- src/compiler.js
+++ src/compiler.js
@@ -32,7 +32,7 @@ function processTag(line) {
     const tag = {};
     
     const comment = line.match(/\;.*/);
-    if (comment) line.replace(comment, "");
+    if (comment) line.replace(comment[0], "");
 
     const org = line.match(/org (\d+h?)/);
     if (org) {
